4e0263399e23880da97ed6a0bc1ee7ab5dd035cf
Vy Bui Nguyen <buing004@umn.edu>
2026-04-16 15:20:59 -0500

    fix base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/vsam-election-calculator/', // set the base path for GitHub Pages deployment
 })
